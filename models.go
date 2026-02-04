2805fcad34307415c1f887b1bd3c28e6d21e6a8b
Lucas Lamisse <22200972t>
2026-02-04 16:10:45 +0100

    Définition de la structure des données

---
+++ models.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"math"
+)
+
+
+
+// Constantes et Enumérations
+
+type NodeType int
+
+const (
+	TypeHotel NodeType = iota
+	TypeSite
+)
+
+
+
+// Définition du Problème
+
+type Point struct {
+	ID          int
+	Type        NodeType
+	X, Y        float64
+	Score       float64
+	OpenTime    float64
+	CloseTime   float64
+	ServiceTime float64
+}
+
+type Instance struct {
+	Name string
+
+	NbDays  int
+	MaxDist float64
+
+	Points []*Point
+	
+	HotelIDs []int
+	SiteIDs  []int
+
+	DistMatrix [][]float64
+
+	StartHotelID int
+	EndHotelID   int
+}
+
+
+
+// Définition de la Solution
+
+type Step struct {
+	PointID    int
+	Arrival    float64
+	Wait       float64
+	Departure  float64
+	DistFromPrev float64
+}
+
+type DayTour struct {
+	Steps      []Step
+	DistTotal  float64
+	TimeTotal  float64
+}
+
+type Solution struct {
+	Instance *Instance
+	Days     []DayTour
+	
+	TotalScore float64
+	TotalDist  float64
+}
+
+
+
+// Helpers
+
+func NewInstance(nbPoints int) *Instance {
+	inst := &Instance{
+		Points:     make([]*Point, nbPoints),
+		DistMatrix: make([][]float64, nbPoints),
+		HotelIDs:   make([]int, 0),
+		SiteIDs:    make([]int, 0),
+	}
+	for i := range inst.DistMatrix {
+		inst.DistMatrix[i] = make([]float64, nbPoints)
+	}
+	return inst
+}
+
+func Distance(p1, p2 *Point) float64 {
+	return math.Sqrt(math.Pow(p1.X-p2.X, 2) + math.Pow(p1.Y-p2.Y, 2))
+}
+
+func (inst *Instance) ComputeDistMatrix() {
+	for i := 0; i < len(inst.Points); i++ {
+		for j := 0; j < len(inst.Points); j++ {
+			if i == j {
+				inst.DistMatrix[i][j] = 0
+			} else {
+				dist := Distance(inst.Points[i], inst.Points[j])
+				inst.DistMatrix[i][j] = dist
+				inst.DistMatrix[j][i] = dist
+			}
+		}
+	}
+}
