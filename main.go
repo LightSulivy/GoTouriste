b13cbfed8e6cc5d4a63c6f7aed5d1754e4cb0f7b
LightSulivy <bast91.teisseire@gmail.com>
2026-02-18 16:22:38 +0100

    Main

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"path/filepath"
+)
+
+func main() {
+	// Exemple avec Inst1.txt
+	instancePath := filepath.Join("instances", "Inst1.txt")
+
+	instance, err := LoadInstance(instancePath)
+	if err != nil {
+		log.Fatalf("Erreur chargement instance %s: %v", instancePath, err)
+	}
+
+	// Calcul des stats pour l'affichage demandé
+	nbSites := 0
+	nbHotels := 0
+	for _, p := range instance.Points {
+		if p.Type == TypeSite {
+			nbSites++
+		} else if p.Type == TypeHotel {
+			nbHotels++
+		}
+	}
+
+	fmt.Printf("Instance chargée : %d sites, %d hôtels, Budget : %.2f\n", nbSites, nbHotels, instance.MaxDist)
+}
